23149f5fa81741b5502a94452eb79d9e1b6b4185
Matthew Angelini <matt@lattapartners.com>
2015-11-03 13:29:30 -0500

    Fix bower and gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,6 @@ var gulp = require('gulp');
 // plugins
 var connect = require('gulp-connect');
 var jshint = require('gulp-jshint');
-var uglify = require('gulp-uglify');
-var minifyCSS = require('gulp-minify-css');
 var clean = require('gulp-clean');
 var runSequence = require('run-sequence');
 
@@ -20,19 +18,13 @@ gulp.task('clean', function() {
     gulp.src('./dist/*')
       .pipe(clean({force: true}));
 });
-gulp.task('minify-css', function() {
-  var opts = {comments:true,spare:true};
-  gulp.src(['./app/**/*.css', '!./app/bower_components/**'])
-    .pipe(minifyCSS(opts))
-    .pipe(gulp.dest('./dist/'))
+gulp.task('copy-js-files', function() {
+  gulp.src('./app/js/**')
+    .pipe(gulp.dest('dist/js'));
 });
-gulp.task('minify-js', function() {
-  gulp.src(['./app/**/*.js', '!./app/bower_components/**'])
-    .pipe(uglify({
-      // inSourceMap:
-      // outSourceMap: "app.js.map"
-    }))
-    .pipe(gulp.dest('./dist/'));
+gulp.task('copy-css-files', function() {
+  gulp.src('./app/css/**')
+    .pipe(gulp.dest('dist/css'));
 });
 gulp.task('copy-bower-components', function () {
   gulp.src('./app/bower_components/**')
@@ -63,6 +55,6 @@ gulp.task('serve',
 gulp.task('build', function() {
   runSequence(
     ['clean'],
-    ['lint', 'minify-css', 'minify-js', 'copy-html-files', 'copy-bower-components', 'connectDist']
+    ['lint', 'copy-js-files', 'copy-css-files', 'copy-html-files', 'copy-bower-components', 'connectDist']
   );
 });
